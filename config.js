b842ae70f89d914d0b42c8e1175600e67c81c9d2
qulub02 <istikhomah081@gmail.com>
2025-12-03 16:19:30 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "TOKEN_BOT",  // Ubah Jadi Token Bot Mu !!!
+  owner: "ID_OWNER", // Ubah Jadi Id Mu !!!
+  port: "1466", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "167.172.64.240" // Ubah Jadi Ip Vps Mu !!!
+};
